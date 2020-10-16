c4417a6bc9805cc319f4b1780bc911fc6737d8ca
Sven Ziurawski <sven.ziurawski@peakconcepts.de>
2020-10-16 16:52:20 +0200

    in arbeit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,45 @@
 import React, {useState} from 'react';
-import {FaBars, FaTimes } from 'react-icons/fa';
-import { Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon } from './Navbar.elements';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
+import { Button } from '../../globalStyles';
+import { 
+  Nav, 
+  NavbarContainer, 
+  NavLogo, 
+  NavIcon, 
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavItemBtn,
+  NavBtnLink
+} from './Navbar.elements';
 
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
 
+
+  const showButton = () => {
+    if(window.innerWidth <= 960){
+      setButton(false)
+    }else{
+      setButton(true)
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+
+
   return (
     <>
+    <IconContext.Provider value={ {color: '#fff'}}>
       <Nav>
         <NavbarContainer>
           <NavLogo to="/">
@@ -19,10 +49,33 @@ const Navbar = () => {
           <MobileIcon onClick={handleClick}>
             {click ? <FaTimes /> : <FaBars />}
           </MobileIcon>
+          <NavMenu onClick={handleClick} click={click}>
+            <NavItem>
+              <NavLinks to='/'>Home</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='/services'>Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='/products'>Products</NavLinks>
+            </NavItem>
+            <NavItemBtm>
+              {button ? (
+                <NavBtnLink to="/sign-up">
+                  <Button primary>SIGN UP</Button>
+                </NavBtnLink>
+              ) : (
+                <Button fontBig primary>
+                  SIGN UP
+                </Button>
+              )}
+            </NavItemBtm>
+          </NavMenu>
         </NavbarContainer>
       </Nav>
+      </IconContext.Provider>
     </>
   );
-};
+}
 
 export default Navbar;
--- src/globalStyles.js
+++ src/globalStyles.js
@@ -24,7 +24,27 @@ padding-left: 30px;
 }
 `;
 
+export const Button = styled.button`
+border-radius: 4px;
+background: ${({primary}) => (primary ? '#4B59F7' : '#0467FB') };
+white-space: nowrap;
+padding: ${({big}) => (big ? '12px 664px' : '10px 20px' )};
+color: #fff;
+font-size: ${({fontBig}) => (fontBig ? '20px' : '16px')};
+outline: none;
+border: none;
+cursor: pointer;
 
+&:hover {
+    transition: all0.3s ease-out;
+    background: #fff;
+    background: ${({primary}) => (primary ? '#4B59F7' : '#0467FB') };
+}
+
+@media screen and (max-width: 960px) {
+    width: 100%auto;
+}
+`;
 
 
 
